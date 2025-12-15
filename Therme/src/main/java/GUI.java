966cedbbc0c10a702b05bd159079c476682ebc05
jessicasuessmuth <jessica.suessmuth@student.hnu.de>
2025-12-15 16:39:59 +0100

    Commit

---
+++ Therme/src/main/java/GUI.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class GUI {
+    private JLabel lblStandortauswahl;
+    private JComboBox cbStandortwahl;
+    private JLabel lblBarrierefreiheit;
+    private JLabel lblPersonenanzahl;
+    private JTextField txtPersonenanzahl;
+    private JComboBox cbAufenthaltsdauerTherme;
+    private JLabel lblTarifauswahl;
+    private JRadioButton rbErwachsen;
+    private JRadioButton rbErmäßigt;
+    private JLabel lblÜbernachtung;
+    private JRadioButton rbÜbernachtung;
+    private JRadioButton rbkeineÜbernachtung;
+    private JLabel lblAufenthaltsdauerTherme;
+    private JLabel lblAufenthaltsdauerÜbernachtung;
+    private JTextField txtAufenthaltsdauerÜbernachtung;
+    private JLabel lblZimmerkategorie;
+    private JComboBox cbZimmerkategorie;
+    private JButton cbPreisberechnung;
+}
